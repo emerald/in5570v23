566a187aeb6b2cc33a1e71090f385ea43994c84a
Joachim <joachim.tilsted@gmail.com>
2023-02-09 07:51:44 +0100

    kilroy

---
+++ exercises/session-2/kilroy.m
@@ -0,0 +1,31 @@
+
+%                           - -
+%                         { 0 0 }
+% +===================oOO===(_)===OOo===============+
+const WotchIt <-
+  object program
+    process
+
+      % Greetings.
+      const home <- locate self
+      stdout.putstring[ "Starting at " || home$LNN.asstring || "\n" ]
+
+      % A counter, and a list of places to go.
+      var network : NodeList <- home$activenodes
+
+      % Meet up all nodes in the network.
+      for contact in network
+        const here <- locate self
+        const host <- contact$theNode
+        % Remote Method invocation.
+        host$stdout.putstring["Comming over. Kind regards, Kilroy\n"]
+      end for
+
+      % Back to the begining.
+      move self to home
+
+      % Report.
+      stdout.putstring["I am done\n"]
+
+    end process
+  end program
