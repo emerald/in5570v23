dab9fbcd4c91b2c6eca6b5712c5d5824f36fc793
Joachim Tilsted Kristensen <joachim.tilsted@gmail.com>
2025-02-14 11:08:11 +0100

    add docker and make

--- mininet/run.sh
+++ mininet/run.sh
@@ -4,10 +4,11 @@ set -e
 sudo apt install mininet
 sudo apt install wireshark
 sudo apt install python3
+sudo apt install docker
 sudo apt install build-essential
 
 # run the script.
 sudo mn --custom ./config.py --topo in5570 -x
 
 # perform cleanup (not strictly nessesary).
-# sudo mn -c
+sudo mn -c
